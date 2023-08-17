bbce40af4e1038441d7ec0bbba2016d3d2443d09
Sumanjit38 <sumanjit38@gmail.com>
2023-08-17 11:42:24 +0530

    Create Controller Function

---
+++ backend/controllers/WorkoutController.js
@@ -0,0 +1,52 @@
+const Workout = require('../models/WorkoutModel')
+const mongoose = require('mongoose')
+
+// Get all workouts
+const getWorkouts = async (req, res) => {
+    const workouts = await Workout.find({}).sort({createdAt: -1})
+
+    res.status(200).json(workouts)
+}
+
+// Get a single workout
+const getWorkout = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such workout'})
+    }
+
+    const workout = await Workout.findById(id)
+
+    if (!workout) {
+        return res.status(404).json({error: 'No such workout'})
+    }
+
+    res.status(200).json(workout)
+}
+
+// Create a new workout
+const createWorkout = async (req, res) => {
+    const {title, load, reps} = req.body
+
+    // Add doc to DB
+    try {
+        const workout = await Workout.create({title, load, reps})
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+}
+
+
+// Delete a workout
+
+
+// Update a workout
+
+
+module.exports = {
+    getWorkouts,
+    getWorkout,
+    createWorkout
+}
